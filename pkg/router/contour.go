1467e22450899bfd0469d0210728f3776a0d7aab
brandoncate <brandon.cate@shipt.com>
2022-05-25 10:41:01 -0500

    fix contour httpproxy annotations overwritten if ingress class selected
    Signed-off-by: brandoncate <brandon.cate@shipt.com>

--- pkg/router/contour.go
+++ pkg/router/contour.go
@@ -188,9 +188,7 @@ func (cr *ContourRouter) Reconcile(canary *flaggerv1.Canary) error {
 		}
 
 		if cr.ingressClass != "" {
-			proxy.Annotations = map[string]string{
-				annotation: cr.ingressClass,
-			}
+			proxy.Annotations[annotation] = cr.ingressClass
 		}
 
 		_, err = cr.contourClient.ProjectcontourV1().HTTPProxies(canary.Namespace).Create(context.TODO(), proxy, metav1.CreateOptions{})
